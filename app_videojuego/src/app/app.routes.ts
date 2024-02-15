0aa6a32e6334df6bec4652bb454c5c91501ab3bc
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2024-02-14 23:13:39 -0500

    Funciono Loka

--- app_videojuego/src/app/app.routes.ts
+++ app_videojuego/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegiterComponent } from './regiter/regiter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: "/inicio", pathMatch : "full"},
+    {path: 'inicio', component: UserComponent},
+    {path: 'registro', component: RegiterComponent}
+];
