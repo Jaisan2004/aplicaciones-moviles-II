6b58d4ac5e9e71bc8a46091613670f24fcc30fde
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2024-02-15 01:35:23 -0500

    productos

--- app_videojuego/src/app/app.routes.ts
+++ app_videojuego/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { RegiterComponent } from './regiter/regiter.component';
+import { ProductsComponent } from './products/products.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "/inicio", pathMatch : "full"},
     {path: 'inicio', component: UserComponent},
-    {path: 'registro', component: RegiterComponent}
+    {path: 'registro', component: RegiterComponent},
+    {path: 'productos', component: ProductsComponent}
 ];
